44c8f1f9fa3f2091632307a04a489e34b9e4c6f1
IT20198404 <89381523+IT20198404@users.noreply.github.com>
2021-09-26 14:05:01 +0530

    one

---
+++ app - Copy/src/main/java/com/example/juice_delivery_app/driver_reg_profile.java
@@ -0,0 +1,14 @@
+package com.example.juice_delivery_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class driver_reg_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_reg_profile);
+    }
+}
